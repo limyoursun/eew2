bce56e8ef1c5073f565c58ff9ac9402b324a1a72
limyoursun <limyoursun@gmail.com>
2021-10-05 14:19:01 +0900

    done

--- src/js/ui_common.js
+++ src/js/ui_common.js
@@ -44,3 +44,29 @@ $(function () {
     $(this).parent('p').nextAll('ul').slideToggle('fast');
   })
 })
+
+function autoCrate(cellValue) {
+  var html, txt = "";
+  switch (cellValue) {
+    case "manual":
+      html = "Y";
+      break;
+    case "automatic":
+      html = "N";
+      break;
+    default:
+      html = "";
+  }
+  return html;
+}
+function waveExistence(cellValue) {
+  var html, txt = "";
+  switch (cellValue) {
+    case "y":
+      html = "○";
+      break;
+    default:
+      html = "";
+  }
+  return html;
+}
